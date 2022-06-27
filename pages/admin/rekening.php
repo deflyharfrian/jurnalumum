64c48f477790cf0741cf5fe518e2ed4ce5cb6b35
rhanadam32 <101385487+rhanadam32@users.noreply.github.com>
2022-06-27 21:30:05 +0700

    Update rekening.php

--- pages/admin/rekening.php
+++ pages/admin/rekening.php
@@ -1,11 +1,12 @@
 <?php
 error_reporting(0);
 if (isset($_POST['ok'])) {
-    $a = $_POST['kode_kategori'];
-    $b = $_POST['nama'];
-    $c = $_POST['tanggal_post'];
+    $norek = $_POST['no_rek'];
+    $nama_rek = $_POST['nama_rek'];
+    $saldo = $_POST['saldo'];
+    $tgl_post = $_POST['tanggal_post'];
 
-    mysqli_query($con, "insert into tb_kategori values('$a','$b','$c')");
+    mysqli_query($con, "insert into tb_rekening values('','$norek','$nama_rek', '$saldo', '$tgl_post')");
     echo "<script>alert('data berhasil disimpan')</script>";
 }
 
@@ -15,12 +16,12 @@ if (isset($_POST['ok'])) {
 
 <div class="row mb-2">
     <div class="col-sm-6">
-        <h1 class="m-0">Kategori</h1>
+        <h1 class="m-0">Rekening</h1>
     </div><!-- /.col -->
     <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Kategori</li>
+            <li class="breadcrumb-item active">Rekening</li>
         </ol>
     </div><!-- /.col -->
 </div><!-- /.row -->
@@ -34,36 +35,38 @@ if (isset($_POST['ok'])) {
 
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Tabel Data Kategori</h3>
+                <h3 class="card-title">Tabel Data Rekening</h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-                <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#mdltambahkategori">
-                    <i class="fa fa-plus"></i> Tambah Data
+                <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#mdltambah_rekening">
+                    <i class="fa fa-plus"></i> Tambah Rekening
                 </button>
 
                 <table id="example1" class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th>No</th>
-                            <th>Kode Kategori</th>
-                            <th>Kategori</th>
-                            <th>Aksi</th>
+                            <th>No Rekening</th>
+                            <th>Nama Rekening</th>
+                            <th>Nominal (Rp)</th>
+                            <th>tanggal ditambahkan</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
                         $i = 1;
-                        $sql = mysqli_query($con, "SELECT * FROM tb_kategori");
+                        $sql = mysqli_query($con, "SELECT * FROM tb_rekening");
                         while ($data = mysqli_fetch_array($sql)) {
                         ?>
                             <tr>
                                 <td><?= $i++; ?></td>
-                                <td><span class="badge badge-primary"><?= $data['kode_kategori'] ?></span>
-                                </td>
-                                <td><?= $data['nama'] ?></td>
+                                <td><?= $data['no_rek'] ?></td>
+                                <td><?= $data['nama_rek'] ?></td>
+                                <td><?= $data['nominal'] ?></td>
+                                <td><?= $data['tgl_post'] ?></td>
                                 <td>
-                                    <a onclick="return confirm('yakin ingin menghapus data ini?');" href="hapus_kategori.php?kode_kategori=<?= $data['kode_kategori'] ?>" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+                                    <a onclick="return confirm('yakin ingin menghapus data ini?');" href="hapus_kategori.php?kode_kategori=<?= $data['kode_kategori'] ?>" class="btn btn-danger"><i class="fa fa-trash"></i> </a>
                                     <a data-toggle="modal" data-target="#mdleditkategori<?= $data['kode_kategori'] ?>" href="#mdleditkategori<?= $data['kode_kategori'] ?>" class="btn btn-warning"><i class="fa fa-edit"></i></a>
                                     <button class="btn btn-success"><i class="fa fa-print"></i></button>
                                 </td>
@@ -78,11 +81,11 @@ if (isset($_POST['ok'])) {
         <!-- /.card -->
 
         <!-- Modal TAMBAH -->
-        <div class="modal fade" id="mdltambahkategori" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="mdltambah_rekening" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header bg-primary">
-                        <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-plus"></i> Tambah Data Kategori</h5>
+                        <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-plus"></i> Tambah Rekening</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -90,18 +93,18 @@ if (isset($_POST['ok'])) {
                     <div class="modal-body">
                         <form method="POST">
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Kode Kategori</label>
-                                <input type="text" class="form-control" name="kode_kategori" placeholder="Kode Kategori">
+                                <label for="exampleInputEmail1">Nomor Rekening</label>
+                                <input type="text" class="form-control" name="no_rek" placeholder="Nomor Rekening">
 
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Kategori</label>
-                                <input type="text" class="form-control" name="nama" placeholder="Nama Kategori">
+                                <label for="exampleInputPassword1">Nama Rekening</label>
+                                <input type="text" class="form-control" name="nama_rek" placeholder="Nama Rekening">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Tanggal Post</label>
-
-                                <input type="text" class="form-control" readonly name="tanggal_post" value="<?= date('Y-m-d H:i') ?>" placeholder="Nama Kategori">
+                                <label for="exampleInputPassword1">Nominal Awal / Saldo</label>
+                                <input type="text" class="form-control" name="saldo" placeholder="Nominal Awal / Saldo">
+                                <input type="text" class="form-control" hidden readonly name="tanggal_post" value="<?= date('Y-m-d H:i:s') ?>" placeholder="Nama Kategori">
                             </div>
 
                     </div>
