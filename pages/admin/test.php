f5cd638fc3298c12be00085e9abee842f8b48fcf
rhanadam32 <101385487+rhanadam32@users.noreply.github.com>
2022-06-28 09:43:45 +0700

    Delete test.php

--- pages/admin/test.php
+++
@@ -1,9 +0,0 @@
-<div class="form-group">
-                                <label for="exampleInputPassword1"> Kategori</label>
-                                <select name="kategori" class="form-control" id="kategori">
-                                <?  $sql_kategori = mysqli_query($con, "SELECT * FROM tb_kategori "); 
-                                    while($data_kategori = mysqli_fetch_array($sql_kategori)){ ?> 
-                                    <option value=""><?php echo $data_kategori[1];?>-</option>
-                                    <? } ?>
-                                </select> 
-                            </div>    
