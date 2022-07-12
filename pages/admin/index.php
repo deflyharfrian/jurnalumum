839f597ca2c0a8a86835dbf3b8afaf173dd7836a
Defly Harfrian <deflyharfrian@gmail.com>
2022-07-12 12:35:32 +0700

    Update index.php
    UPDATE LIGHT THEME

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -115,11 +115,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-light-primary elevation-4">
       <!-- Brand Logo -->
       <a href="index3.html" class="brand-link">
         <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Jurnal Umum</span>
+        <span class="brand-text font-weight-light"><b>Jurnal Umum</b></span>
       </a>
 
       <!-- Sidebar -->
