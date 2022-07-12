7ecbd2f0b7c25f4a4045036435ac359f23790e35
Defly Harfrian <deflyharfrian@gmail.com>
2022-07-12 12:28:35 +0700

    Update index.php
    update nama SESSION setelah Login berdasarkan LEVEL

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -10,7 +10,9 @@ session_start();
 if ($_SESSION['level'] == "") {
   header("location:../../index.php?pesan=belum_login");
 }
-
+$aa = $_SESSION['username'];
+$p = mysqli_query($con,"SELECT * FROM tb_user WHERE username='$aa'");
+$profil = mysqli_fetch_array($p);
 ?>
 
 
@@ -117,7 +119,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Brand Logo -->
       <a href="index3.html" class="brand-link">
         <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Jurnal Umum</span>
       </a>
 
       <!-- Sidebar -->
@@ -128,7 +130,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
-            <a href="#" class="d-block">Alexander Pierce</a>
+            <a href="#" class="d-block"><?php echo $profil['nama_lengkap']; ?></a>
           </div>
         </div>
 
