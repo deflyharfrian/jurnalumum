8eec980984d3f9584a3480389f4a0521f77f9551
Defly Harfrian <deflyharfrian@gmail.com>
2022-07-12 12:45:02 +0700

    Update index.php
    UPDATE MENU ACTIVEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -320,7 +320,23 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
   <script>
-    $(function() {
+$(function () {
+    var url = window.location;
+    // for single sidebar menu
+    $('ul.nav-sidebar a').filter(function () {
+        return this.href == url;
+    }).addClass('active');
+
+    // for sidebar menu and treeview
+    $('ul.nav-treeview a').filter(function () {
+        return this.href == url;
+    }).parentsUntil(".nav-sidebar > .nav-treeview")
+        .css({'display': 'block'})
+        .addClass('menu-open').prev('a')
+        .addClass('active');
+});
+
+$(function() {
       $("#example1").DataTable({
         "responsive": true,
         "lengthChange": false,
